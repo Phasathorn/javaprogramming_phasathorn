b7405a742d61dbc9ce9bfaf4221e4d17ea0cd413
Phasathorn seesaard <mark17137@hotmail.com>
2022-12-16 11:39:31 +0700

    16/12/65 first commit

---
+++ example.java
@@ -0,0 +1,8 @@
+public class example{
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+	public void myMethod(){
+		 System.out.println("Phasathorn Seesaard");
+	}
+}
